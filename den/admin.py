0346194c686b313e410600f913785a8fdee75900
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-10-14 23:30:02 +0600

    delete

--- den/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from den.models import Problem, Action
-
-admin.site.register(Problem)
-admin.site.register(Action)
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from user.views import user_login , register
-
-urlpatterns = [
-	path('login/', user_login),
-	path('register/', register),
-]
